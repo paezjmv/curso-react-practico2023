fc47843d3ae623e626f2835586cdbfadc3cc21a4
Martin Paez <martinpaez@MacBook-Air-de-Martin.local>
2025-11-04 18:57:45 -0600

    [Add] ShoppingCart Component

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import ShoppingCart from '../ShoppingCart/'
 
 const Navbar = () => {
   const context = useContext(ShoppingCartContext)
@@ -142,8 +143,7 @@ const renderView = () => {
       <ul className='flex items-center gap-3'>
         {renderView()}
         <li className='flex items-center'>
-          <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon>
-          <div>{context.cartProducts.length}</div>
+          <ShoppingCart />
         </li>
       </ul>
     </nav>
--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState, useRef } from 'react'
 import { Link, Navigate } from 'react-router-dom'
-import { ShoppingCartContext } from '../../Context/'
+import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function SignIn() {
